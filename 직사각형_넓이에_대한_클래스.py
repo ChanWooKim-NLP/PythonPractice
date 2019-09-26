d222e0e07d6c47e03fb9eb432238f44fdc45b031
김찬우 <50550972+Tobeanengineer@users.noreply.github.com>
2019-09-26 23:13:01 +0900

    9.26 클래스 개념 연습
    self와 생성자에 대해 더 알아갈 수 있었음
    객체지향 프로그램에 대한 입문

---
+++ 직사각형_넓이에_대한_클래스.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 26 22:10:50 2019
+
+@author: Kim Chanwoo
+"""
+
+class Rectangle:
+    def __init__(self, x1, y1, x2, y2):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        
+    def check(self):
+        if self.x1 == self.x2 or self.y1 == self.y2:
+            return False
+        else: return True
+        
+    def area(self):
+        return abs(self.x1-self.x2)*abs(self.y1-self.y2)
+    
+    def __call__(self):
+        if self.x1 == self.x2 or self.y1 == self.y2:
+            return "출력 불가"
+        else:
+            return (self.x1, self.y1)
+            return (self.x2, self.y2)
+            return Rectangle.area()
+        
+    def equal(self, x3, y3):
+        self.x3 = x3
+        self.y3 = y3
+        if (x3 == self.x1 or self.x2) and (y3 == self.y1 or self.y2):
+            return True
+        else:
+            if abs(x3-self.x1)*abs(y3-self.y1) == Rectangle.area():
+                return True
+            elif abs(x3-self.x2)*abs(y3-self.y2) == Rectangle.area():
+                return True
+            else:
+                return False
+            
---
+++ 클래스를_이용한_좌표평면_기초.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 24 21:48:34 2019
+
+@author: Kim Chanwoo
+"""
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def __call__(self):
+        return (self.x, self.y)
+        
+    def equal(self, x1, y1):
+        self.x1 = x1
+        self.y1 = y1
+        if (self.x1, self.y1) == (self.x, self.y):
+            return 1
+        else: return 0
+        
+    def distance_from_origin(self):
+        return (self.x**2 + self.y**2)**0.5
+    
+    def distance_from_point(self):
+        return ((self.x-self.x1)**2 + (self.y-self.y1)**2)**0.5
