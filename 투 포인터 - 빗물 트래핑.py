0cef18b1e33ca9ec42b0a2d47de28a5f8bffe498
김찬우 <50550972+Tobeanengineer@users.noreply.github.com>
2020-11-26 20:25:53 +0900

    Add files via upload
    투 포인터를 이용한 빗물의 면적 구하기

---
+++ 투 포인터 - 빗물 트래핑.py
@@ -0,0 +1,25 @@
+# 투 포인터 이용
+
+def solution1(height):
+    if not height:
+        return 0
+
+    volume = 0
+    left, right = 0, len(height) - 1
+    left_max, right_max = height[left], height[right] # 현재 위치
+    # 높은 쪽을 향해서 포인터가 이동함. 민약 왼쪽이 높으면 오른쪽에서 하나 깎음
+    while left < right: # 포인터가 역전되면 안됨
+        left_max, right_max = max(height[left], left_max),\
+                                max(height[right], right_max)
+
+        if left_max <= right_max:
+            volume += left_max - height[left]
+            left += 1
+
+        else:
+            volume += right_max - height[right]
+            right -= 1
+
+    return volume
+
+
