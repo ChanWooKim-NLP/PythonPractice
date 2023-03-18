9f28f7a3d4ba431ef57c4db40c406eb4f4f28782
김찬우 <50550972+KimChanw@users.noreply.github.com>
2023-03-19 04:13:00 +0900

    [Gold III] Title: 코딩은 예쁘게, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/2879. 코딩은 예쁘게/코딩은 예쁘게.py
+++ 백준/Gold/2879. 코딩은 예쁘게/코딩은 예쁘게.py
@@ -22,22 +22,24 @@ while stack:
     if top == 0:
         continue
     
-    # 수정해야 할 횟수, top이 음수면 제일 큰 값 / 양수면 제일 작은 값으로 갱신
+    # 수정해야 할 횟수
     # temp_arr에 일관적으로 더하거나 빼준 후 다시 stack에 push함
     indent_cnt = top
     
-    # temp : 현재 스택 상태 top, 이전에 꺼낸 top과 변수가 같으면 temp_arr에 저장
+    # temp : 현재 스택의 top과 방금 꺼낸 top 간 부호가 같으면 temp_arr에 저장
+    # 부호가 같다 = 두 수의 곱이 양수
     while stack and stack[-1] * top > 0:
         temp = stack.pop()
         temp_arr.append(temp)
         
+        # top이 음수면 제일 큰 값 / 양수면 제일 작은 값으로 갱신
         if top < 0:
             indent_cnt = max(indent_cnt, temp)
             
         else:
             indent_cnt = min(indent_cnt, temp)
         
-    # 같은 부호를 가진 값을 저장한 배열 길이만큼 pop -> ident_cnt와 연산 -> stack에 push
+    # 같은 부호를 가진 값을 저장한 배열 길이만큼 pop -> ident_cnt와 연산 -> stack에 다시 push
     for _ in range(len(temp_arr)):
         after_correction_cnt = temp_arr.pop() - indent_cnt
         stack.append(after_correction_cnt)
